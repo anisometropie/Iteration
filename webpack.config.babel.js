83e741e07afcb41511a24e60b80af32ff0dc1da6
Valentin Chanas <anisometropie@gmail.com>
2019-11-16 23:41:29 +0100

    configure webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules=true']
+      }
+    ]
+  },
+  mode: 'development'
+}
