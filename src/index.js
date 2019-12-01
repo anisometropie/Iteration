3da6793eb291b68bb7c7eb5076f0f3dffc149424
Valentin Chanas <anisometropie@gmail.com>
2019-12-01 01:33:47 +0100

    setup and drawing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import Complex from 'simple-complex'
-
 import Graph from './Graph'
-import Vector from './Vector'
 
+const ITERATIONS = 1000
 const canvas = document.getElementById('canvas')
-const graph = new Graph(-1, 1, -1, 1, canvas)
+const graph = new Graph(-2, 1, -1, 1, canvas)
 
-const mouse = new Complex(0, 0)
+const iterate = z => Complex.sum(Complex.square(z), graph.mouse)
+const generateValues = (values = []) => {
+  for (let i = 0; i < ITERATIONS; i++) {
+    const { real, imaginary } = iterate(values[i])
+    values[i + 1].set(real, imaginary)
+  }
+}
+const values = [new Complex(0, 0), ...Array(ITERATIONS).keys()].map(
+  n => new Complex()
+)
 
 function draw() {
   window.requestAnimationFrame(draw)
   graph.clear()
+  graph.drawAxes()
+  graph.drawPoint(graph.mouse)
+  generateValues(values)
+  values.forEach((v, i) => {
+    graph.drawLine(v, values[i + 1])
+    graph.drawPoint(v)
+  })
 }
 draw()
