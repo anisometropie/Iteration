cdb6001ee08ab693dd456d42adec262bfeab5406
Valentin Chanas <anisometropie@gmail.com>
2020-05-06 00:19:02 +0200

    simpler express config

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('public/index.html'))
-})
+app.use('/', express.static('public'))
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve('public/index.html'))
+// })
 
 console.log(`listening to port ${port}`)
 app.listen(port)
